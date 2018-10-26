f0d4980346ae461d8c4f82023b2c5d3c6708ce3e
kensuke.saito <kensuke.saito@persol.co.jp>
2018-10-27 00:33:02 +0900

    append minimum function.

---
+++ index.js
@@ -0,0 +1,25 @@
+// LINE用botのプロトタイプ
+
+// ================================
+// ライブラリインポート
+const server = require('express')();
+const line   = require('@line/bot-sdk');  // LINE Messaging-API用のSDK
+
+// ================================
+// LINE接続用パラメータ
+const line_config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+// ================================
+// Webサーバ設定
+server.listen(process.env.PORT || 3000);
+
+// ================================
+// ルーティング設定
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+  res.sendStatus(200);
+  console.log(req.body);
+});
+
