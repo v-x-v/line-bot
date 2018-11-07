97a4b57c0e7c4651118e426bd624e8c44ac54a38
kensuke.saito <kensuke.saito@persol.co.jp>
2018-11-07 15:35:55 +0900

    echo bot append.

--- index.js
+++ index.js
@@ -12,14 +12,39 @@ const line_config = {
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
+const bot = new line.Client(line_config);
+
 // ================================
 // Webサーバ設定
 server.listen(process.env.PORT || 8080);
 
+
 // ================================
 // ルーティング設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+  // まずLINE側にステータスコード200 OKを返す
   res.sendStatus(200);
   console.log(req.body);
+
+  // 全てのイベント処理のプロミスを格納する配列
+  let events_processed = [];
+
+  // イベントオブジェクトを順次処理
+  req.body.events.forEach((event) => {
+    // メッセージがテキストの時のみ
+    if(event.type == 'message' && event.message.type == 'text') {
+        events_processed.push(bot.replyMessage(event.replyToken, {
+          type: 'text',
+          text: event.message.text
+        }));
+    }
+  });
+
+  // すべてのイベント処理が終了されたら、処理数を出力
+  Promise.all(events_processed).then(
+    (response) => {
+      console.log(`${response.length} event(s) processed.`)
+    }
+  );
 });
 
