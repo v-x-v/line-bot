129bd34760b6f52255dc532d23ce7e6a82476ef0
kensuke.saito <kensuke.saito@persol.co.jp>
2018-11-07 19:06:33 +0900

    richmenu append.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // ================================
 // ライブラリインポート
+require('dotenv').config(); // .envファイルから環境変数を読み込む
 const server = require('express')();
 const line   = require('@line/bot-sdk');  // LINE Messaging-API用のSDK
 
@@ -14,11 +15,50 @@ const line_config = {
 
 const bot = new line.Client(line_config);
 
+// リッチメニュー設定
+const richObject = {
+  size: {
+    width: 2500,
+    height: 1686
+  },
+  selected: true,
+  name: 'Sample',
+  chatBarText: 'ひらけごま',
+  areas: [
+    {
+      bounds: { x: 0, y: 0, width: 1250, height: 1686},
+      action: {
+        type: 'postback',
+        data: 'action=1',
+        label: '1番',
+        displayText: '1番を選びます'
+      }
+    },
+    {
+      bounds: { x: 1250, y: 0, width: 1250, height: 1686},
+      action: {
+        type: 'postback',
+        data: 'action=1',
+        label: '1番',
+        displayText: '1番を選びます'
+      }
+    }
+  ]
+};
+let richMenuId;
+bot.createRichMenu(richObject)
+.then((id) => {
+  richMenuId = id;
+  console.log('rich menu id: ' + id);
+});
+
+
 // ================================
 // Webサーバ設定
 server.listen(process.env.PORT || 8080);
 
-
+// テスト接続時に使われるReplyToken
+const testReplyTokenList = ['00000000000000000000000000000000', 'ffffffffffffffffffffffffffffffff']
 // ================================
 // ルーティング設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
@@ -31,20 +71,32 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
 
   // イベントオブジェクトを順次処理
   req.body.events.forEach((event) => {
-    // メッセージがテキストの時のみ
-    if(event.type == 'message' && event.message.type == 'text') {
+    // 管理画面のテスト接続時はトークンが固定。処理しない
+    if(testReplyTokenList.indexOf(event.replyToken) >= 0) {
+      // メッセージがテキストの時のみ
+      if(event.type == 'message' && event.message.type == 'text') {
+        if(event.message.text == 'メニュー') {
+          events_processed.push(bot.getRichMenu(richMenuId));
+        }
         events_processed.push(bot.replyMessage(event.replyToken, {
           type: 'text',
           text: event.message.text
         }));
     }
+
+    }
   });
 
   // すべてのイベント処理が終了されたら、処理数を出力
-  Promise.all(events_processed).then(
+  Promise.all(events_processed)
+  .then(
     (response) => {
       console.log(`${response.length} event(s) processed.`)
     }
-  );
+  )
+  .catch((err) => {
+    // 途中でエラーが発生した場合は内容を表示
+    console.log(err);
+  });
 });
 
